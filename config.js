843ca02ae217c6b0e81e27d5c65150d22aae05ab
DominicDev <s0l0estudi4r@gmail.com>
2025-11-02 13:56:31 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'JUNGLE';
+export let nameSongs = [
+	"BUSY EARNIN'", /*1*/
+	"BONNIE HILL", /*2*/
+	"CANDLE FLAME", /*3*/
+	"LUCKY I GOT WHAT I WANT", /*4*/
+	"HOLDING ON", /*5*/
+	"BACK ON 74", /*6*/
+	"PALM TREES", /*7*/
+	"CASIO", /*8*/
+	"BEAT 54 (ALL GOOD NOW)", /*9*/
+	"HEAVY, CALIFORNIA", /*10*/
+	"CHERRY", /*11*/
+	"ALL OF THE TIME", /*12*/
+	"KEEP MOVING", /*13*/
+	"GOOD TIMES", /*14*/
+	"LIFTING YOU", /*15*/
+	"PROBLEMZ", /*16*/
+	"TIME", /*17*/
+	"HAPPY MAN", /*18*/
+	"COSURMYNE", /*19*/
+	"DROPS", /*20*/
+]
